1a322637e54ebc39f7cbb9099de83ce40b573f25
CJ <csh.joo77@gmail.com>
2024-02-26 18:33:40 -0500

    added javascript code to text

--- Assets/script.js
+++ Assets/script.js
@@ -1,6 +1,12 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+$(document).ready(function(){
+  console.log('Ready!');
+
+});
+
+var now = moment().format("dddd, MMMM Do YYYYY");
+var datedisplay = document.getElementById('currentDay');
+datedisplay.innerHTML = now;
+var currenthour = moment().format('HH');
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
